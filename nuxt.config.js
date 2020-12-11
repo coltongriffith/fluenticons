2a6bf2c1cf27793eb56459c2cf86ded8ac611b8b
Fayaz Ahmed <fayazara@gmail.com>
2020-12-11 10:20:21 +0530

    Meta fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,52 @@
-import { defaultHead } from "./constants/index";
+const title = "Fluent Icons - Free & Open Source Icons from Microsoft";
+const description =
+  "MIT open source SVG icons from Microsoft, over 2100 beautiful icoons. Solid and 2px stroke based sets.";
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: "static",
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: defaultHead,
+  head: title,
+  meta: [
+    { charset: "utf-8" },
+    {
+      name: "viewport",
+      content:
+        "width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"
+    },
+    {
+      hid: "description",
+      name: "description",
+      content: description
+    },
+    { name: "keywords", content: "icons, svg icons, free icons, svg icons" },
+    {
+      property: "og:title",
+      name: "og:title",
+      content: title
+    },
+    {
+      property: "og:type",
+      name: "og:type",
+      content: "website"
+    },
+    {
+      property: "og:url",
+      name: "og:url",
+      content: "https://fluenticons.co/"
+    },
+    {
+      property: "og:description",
+      name: "og:description",
+      content: description
+    },
+    {
+      property: "og:site_name",
+      name: "og:site_name",
+      content: "Fluent Icons"
+    }
+  ],
+  link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: ["~/assets/styles/main.css"],
