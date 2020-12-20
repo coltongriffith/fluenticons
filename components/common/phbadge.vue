48b88f02d8258c6b7e106b3290343a978240c5c6
Fayaz Ahmed <fayazara@gmail.com>
2020-12-20 12:03:50 +0530

    Product hunt badge

---
+++ components/common/phbadge.vue
@@ -0,0 +1,14 @@
+<template>
+  <a
+    class="fixed bottom-4 left-4"
+    href="https://www.producthunt.com/posts/fluenticons-viewer?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-fluenticons-viewer"
+    target="_blank"
+    ><img
+      :src="`https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=278669&theme=${$colorMode.value}`"
+      alt="Fluenticons Viewer - 2000+ pixel perfect open source icons | Product
+    Hunt"
+      style="width: 250px; height: 54px"
+      width="250"
+      height="54"
+  /></a>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="antialiased">
     <Nuxt />
+    <phbadge />
   </div>
 </template>
