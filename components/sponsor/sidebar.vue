b2d0e641db9db454963118bc967bf568d01b2b21
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2021-12-01 09:26:06 +0530

    Updated the banner

--- components/sponsor/sidebar.vue
+++ components/sponsor/sidebar.vue
@@ -1,11 +1,11 @@
 <template>
   <a
-    href="https://www.iconfinder.com/discountcode/blackfriday21?utm_campaign=BlackFriday21&utm_medium=Sponsorship&utm_source=Fluenticons&utm_content=Black%20Friday%202021"
+    href="https://www.iconfinder.com/p/illustrations#giftcard-section?utm_campaign=Giftcard&utm_medium=Sponsorship&utm_source=Fluenticons&utm_content=Humanic%20illustrations"
     target="_blank"
     rel="noopener"
   >
     <img
-      src="/iconfinder-blackfriday.png"
+      src="/iconfinder.svg"
       alt="Icon Finder"
       class="absolute bottom-0 left-0 w-full"
     />
