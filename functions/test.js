82e7497115c3f1ba82e6dfa77a8ce15b26808613
fayazara <fayazara@gmail.com>
2022-04-15 17:18:06 +0530

    Removed test files

--- functions/test.js
+++
@@ -1,9 +0,0 @@
-import parser from 'rss-parser';
-export async function onRequest(context) {
-    let feed = await parser.parseURL('https://www.reddit.com/.rss');
-    return new Response(JSON.stringify(feed), {
-        headers: {
-            'content-type': 'application/json;charset=UTF-8',
-        }
-    });
-}
