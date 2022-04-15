a4ee4737a55150e56a1efc2615da712f8161f283
fayazara <fayazara@gmail.com>
2022-04-15 17:08:54 +0530

    cloudflare worker test with a npm package

--- functions/test.js
+++ functions/test.js
@@ -1,20 +1,10 @@
+let Parser = require('rss-parser');
+let parser = new Parser();
 export async function onRequest(context) {
-    // Contents of context object
-    const {
-        request, // same as existing Worker API
-        env, // same as existing Worker API
-        params, // if filename includes [id] or [[path]]
-        waitUntil, // same as ctx.waitUntil in existing Worker API
-        next, // used for middleware or to fetch assets
-        data, // arbitrary space for passing data between middlewares
-    } = context;
-
-    return new Response({
-        request,
-        env,
-        params,
-        waitUntil,
-        next,
-        data,
+    let feed = await parser.parseURL('https://www.reddit.com/.rss');
+    return new Response(JSON.stringify(feed), {
+        headers: {
+            'content-type': 'application/json;charset=UTF-8',
+        }
     });
 }
