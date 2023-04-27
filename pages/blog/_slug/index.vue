cd553508f845a83fccbd0d97feb30cefdfbb345a
excellent-coding <dw951012@gmail.com>
2023-04-26 20:26:22 -0400

    blog-layout

--- pages/blog/_slug/index.vue
+++ pages/blog/_slug/index.vue
@@ -21,27 +21,30 @@
       <Header class="ml-8 text-white" />
     </div> -->
     <div
-      class="relative xs:p-8 lg:p-16 w-full h-full markdown-body post-right custom-scroll"
+      class="relative xs:p-8 lg:p-16 w-full h-full markdown-body post-right custom-scroll max-w-7xl mx-auto p-6"
     >
       <h1 class="font-bold text-4xl text-center">{{ article.title }}</h1>
       <p class="pb-4 text-center">
         Post last updated: {{ formatDate(article.updatedAt) }}
       </p>
       <p class="text-center">{{ article.description }}</p>
-      <img
-        :src="api + article.img"
-        :alt="article.title"
-        class="h-full w-full object-cover"
-      />
-      <div v-if="article.content">
-        <p
-          v-for="(content, i) in article.content.split('\n')"
-          :key="{ i }"
-          class="mb-3"
-        >
-          {{ content }}
-        </p>
+      <div class="max-w-4xl mx-auto">
+        <img
+          :src="api + article.img"
+          :alt="article.title"
+          class="h-full w-full object-cover max-w-lg mx-auto mt-16 mb-10"
+        />
+        <div v-if="article.content">
+          <p
+            v-for="(content, i) in article.content.split('\n')"
+            :key="{ i }"
+            class="mb-3"
+          >
+            {{ content }}
+          </p>
+        </div>
       </div>
+
       <!-- content author component -->
       <!-- <author :author="article.author" /> -->
       <!-- prevNext component -->
